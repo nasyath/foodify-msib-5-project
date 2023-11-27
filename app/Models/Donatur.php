a72d6fadf9288fc0d41ef96e04443a387634a34e
IrgiRamz <irgirama01@gmail.com>
2023-11-27 07:02:20 +0700

    add models

--- app/Models/Donatur.php
+++ app/Models/Donatur.php
@@ -6,14 +6,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class JMakanan extends Model
+
+class Donatur extends Model
 {
     use HasFactory;
-    protected $table = 'tb_jenis_makanan';
-    protected $fillable = ['nama_jenis'];
+    protected $table = 'tb_donatur';
+    protected $fillable = [
+        'nama_donatur','alamat','no_hp','deskripsi','foto'
+    ];
+
+    public $timestamps = false;
 
     public function tb_donasi(): HasMany
     {
         return $this->hasMany(Donasi::class);
-     }
+    }
 }
