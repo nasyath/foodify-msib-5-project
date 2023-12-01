1f74e943e11514752f07b8fc1901a3e1929f55a3
Carmennita <inicarmennita@gmail.com>
2023-12-01 10:19:51 +0800

    rapikan vertical_headbar

--- resources/views/themes/template.blade.php
+++ resources/views/themes/template.blade.php
@@ -94,8 +94,11 @@
     <script src="{{ asset('backend/assets/js/pages/gridjs.init.js') }}"></script>
 
     <script src="{{ asset('backend/assets/js/app.js') }}"></script>
-    
-    
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- data tables || startbootstrap -->
     <script src="//cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
