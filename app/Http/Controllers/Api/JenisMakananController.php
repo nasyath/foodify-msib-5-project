61ef7f9a3219b0c49d01002feded90de3fdd0dd4
faykarr <nasyathfaykar@gmail.com>
2023-12-10 20:31:20 +0700

    feat: add REST API CRUD #not yet completed

---
+++ app/Http/Controllers/Api/JenisMakananController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\JMakanan;
+use App\Http\Resources\JenisMakananResource;
+use Illuminate\Support\Facades\Validator;
+
+class JenisMakananController extends Controller
+{
+    // function index to get all data from table tb_jenis_makanan
+    public function index()
+    {
+        $data = JMakanan::all();
+        // return to new JenisMakananResource with status, message, and data
+        return new JenisMakananResource(true, 'Data berhasil diambil', $data);
+    }
+
+    // show function to get data by id
+    public function show($id)
+    {
+        $data = JMakanan::find($id);
+        // return to new JenisMakananResource with status, message, and data
+        return new JenisMakananResource(true, 'Data berhasil diambil', $data);
+    }
+
+    // store function to store data to table tb_jenis_makanan
+    public function store(Request $request)
+    {
+        // validate request with validator
+        $request->validate([
+            'nama_jenis' => 'required'
+        ]);
+
+        // Add error message to validator
+        $validator = Validator::make($request->all(), [
+            'nama_jenis' => 'required'
+        ]);
+
+        // if validator is fails return to json error
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // create new JMakanan
+        $data = JMakanan::create([
+            'nama_jenis' => $request->nama_jenis
+        ]);
+
+        // return to new JenisMakananResource with status, message, and data
+        return new JenisMakananResource(true, 'Data berhasil ditambahkan', $data);
+    }
+
+    // update function to update data by id
+    public function update(Request $request, $id)
+    {
+        // validate request
+        $request->validate([
+            'nama_jenis' => 'required'
+        ]);
+
+        // find JMakanan by id
+        $data = JMakanan::find($id);
+
+        // update JMakanan
+        $data->update([
+            'nama_jenis' => $request->nama_jenis
+        ]);
+
+        // return to new JenisMakananResource with status, message, and data
+        return new JenisMakananResource(true, 'Data berhasil diupdate', $data);
+    }
+
+    // destroy function to delete data by id
+    public function destroy($id)
+    {
+        // find JMakanan by id
+        $data = JMakanan::find($id);
+
+        // delete JMakanan
+        $data->delete();
+
+        // return to new JenisMakananResource with status, message, and data
+        return new JenisMakananResource(true, 'Data berhasil dihapus', null);
+    }
+
+    // search function to search data by nama_jenis
+    public function search(Request $request)
+    {
+        // validate request
+        $request->validate([
+            'nama_jenis' => 'required'
+        ]);
+
+        // find JMakanan by nama_jenis
+        $data = JMakanan::where('nama_jenis', 'like', '%' . $request->nama_jenis . '%')->get();
+
+        // return to new JenisMakananResource with status, message, and data
+        return new JenisMakananResource(true, 'Data berhasil ditemukan', $data);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\JenisMakananController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// For API Jenis Makanan
+Route::get('/jenis-makanan', [JenisMakananController::class, 'index']);
+Route::get('/jenis-makanan/{id}', [JenisMakananController::class, 'show']);
+Route::post('/jenis-makanan-store', [JenisMakananController::class, 'store']);
